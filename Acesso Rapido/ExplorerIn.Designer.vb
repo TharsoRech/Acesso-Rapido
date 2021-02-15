61b36132e641c3b8974c22d22c3c975c61d66c93
TharsoRech <77845449+TharsoRech@users.noreply.github.com>
2021-02-15 10:02:10 -0300

    Add files via upload

---
+++ Acesso Rapido/ExplorerIn.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class ExplorerIn
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.ShellView1 = New GongSolutions.Shell.ShellView()
+        Me.FileDialogToolbar1 = New GongSolutions.Shell.FileDialogToolbar()
+        Me.SuspendLayout()
+        '
+        'ShellView1
+        '
+        Me.ShellView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ShellView1.Location = New System.Drawing.Point(0, 31)
+        Me.ShellView1.Name = "ShellView1"
+        Me.ShellView1.Size = New System.Drawing.Size(1061, 708)
+        Me.ShellView1.StatusBar = Nothing
+        Me.ShellView1.TabIndex = 6
+        Me.ShellView1.Text = "ShellView1"
+        '
+        'FileDialogToolbar1
+        '
+        Me.FileDialogToolbar1.AutoSize = True
+        Me.FileDialogToolbar1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.FileDialogToolbar1.Location = New System.Drawing.Point(0, 0)
+        Me.FileDialogToolbar1.Name = "FileDialogToolbar1"
+        Me.FileDialogToolbar1.ShellView = Me.ShellView1
+        Me.FileDialogToolbar1.Size = New System.Drawing.Size(1061, 31)
+        Me.FileDialogToolbar1.TabIndex = 7
+        '
+        'ExplorerIn
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.ShellView1)
+        Me.Controls.Add(Me.FileDialogToolbar1)
+        Me.Name = "ExplorerIn"
+        Me.Size = New System.Drawing.Size(1061, 739)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Public WithEvents ShellView1 As GongSolutions.Shell.ShellView
+    Public WithEvents FileDialogToolbar1 As GongSolutions.Shell.FileDialogToolbar
+End Class
---
+++ Acesso Rapido/Propriedades.vb
@@ -0,0 +1,92 @@
+﻿
+Public Class Propriedades
+    Public IsfolderPR As Boolean = False
+    Public Isfolder As Boolean = False
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+        Try
+            Me.Close()
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Try
+            If Caminho.Text <> "" Then
+                Process.Start(Caminho.Text)
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+
+
+    Private Sub TabControl1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles MetroTabControl1.SelectedIndexChanged
+        Try
+            Dim found As Boolean = False
+            If IsfolderPR = False Then
+                If MetroTabControl1.SelectedTab.Text = "Propriedades" And MetroTextBox1.Text = "" Then
+                    Dim arrHeaders(35)
+                    Dim shell As New Shell32.Shell
+                    Dim objFolder As Shell32.Folder
+                    Dim file As String = Caminho.Text & "\" & Descricao.Text
+                    objFolder = shell.NameSpace(Caminho.Text)
+                    If IO.File.Exists(Caminho.Text & "\" & Descricao.Text) = False Then
+                        If Isfolder Then
+                            objFolder = shell.NameSpace(IO.Path.GetDirectoryName(IO.Path.GetDirectoryName(file)))
+                        Else
+                            objFolder = shell.NameSpace(IO.Path.GetDirectoryName(file))
+                        End If
+                    End If
+                    For i = 0 To 34
+                        arrHeaders(i) = objFolder.GetDetailsOf(objFolder.Items, i)
+                    Next
+                    For Each strFileName In objFolder.Items
+                        For i = 0 To 34
+                            If objFolder.GetDetailsOf(strFileName, i) <> "" Then
+                                If arrHeaders(i).ToString = "Nome" Then
+                                    If Isfolder Then
+                                        If objFolder.GetDetailsOf(strFileName, i) = (Descricao.Text) = False Then
+                                            Exit For
+                                        End If
+                                    Else
+                                        If objFolder.GetDetailsOf(strFileName, i).Contains(Descricao.Text) = False Then
+                                            Exit For
+                                        End If
+                                    End If
+                                End If
+                                MetroTextBox1.Text = MetroTextBox1.Text & arrHeaders(i) & ": " & objFolder.GetDetailsOf(strFileName, i) & Environment.NewLine
+                                End If
+                        Next
+                    Next
+                End If
+            End If
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            Me.Close()
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            If Form1.asadmin = True Then
+                Process.Start(Caminho.Text)
+            Else
+                Login.Show()
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+
+End Class
